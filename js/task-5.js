0a5712c604e8870a226953480bff60277fed3fb7
Zayka Lyudmyla <103557961+ludzay@users.noreply.github.com>
2024-01-28 23:38:27 +0200

    Create task-5.js

---
+++ js/task-5.js
@@ -0,0 +1,16 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+function formHandler(event) {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  spanColor.style.color = color;
+}
+
+button.addEventListener("click", formHandler);
