619b581bc15d8e39d9b4a4221ef5d7017a2b1818
Zayka Lyudmyla <103557961+ludzay@users.noreply.github.com>
2024-02-25 22:50:31 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,16 @@
 const userName = document.getElementById("name-input");
 const userNameOutput = document.getElementById("name-output");
+console.log(userName);
+console.log(userNameOutput);
 
 function inputHandler(event) {
-  userNameOutput.textContent = event.target.value.trim();
+  if (event.target.value.trim() === "") {
+    userNameOutput.textContent = "Anonymous";
+  } else {
+    userNameOutput.textContent = event.target.value.trim();
+  }
+
+  console.log("event.target.value.trim() " + event.target.value.trim());
 }
 
 userName.addEventListener("input", inputHandler);
